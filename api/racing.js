f37cd6c2522d00c4ddab6a5b8b2a7f74cbbd85d6
shane962 <shanesreid@gmail.com>
2025-06-20 13:08:36 +1000

    Update racing.js

--- api/racing.js
+++ api/racing.js
@@ -1,5 +1,4 @@
-
-  }
+ }
   try {
     const sessionToken = process.env.SESSION_TOKEN || "cPDrBLdD8pg0+mDYbEDftYBpX1j1RqHTyfwIuzZpO9M=";
     const sessionToken = process.env.SESSION_TOKEN || "uL6q0PVe7/EAJPxV8ip/1O45Hz7JRv8nU3E9AYQ4K6M=";
